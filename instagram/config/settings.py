8d8d84dc53a012e75cfcedaee677bf5b96120a4b
Kyeong Lee <kaythechemist@gmail.com>
2017-10-13 16:50:10 +0900

    urlpatterns new pattern added and MEDIA URL returns MEDIA files

--- instagram/config/settings.py
+++ instagram/config/settings.py
@@ -19,6 +19,8 @@ ROOT_DIR = os.path.dirname(BASE_DIR)
 CONFIG_SECRET_DIR = os.path.join(ROOT_DIR, '.config_secret')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 
