85c0eb8e8918c71b0f457c085a0053ba26f5e3fb
Kyeong Lee <kaythechemist@gmail.com>
2017-10-16 09:58:04 +0900

    forms.py

--- instagram/post/views.py
+++ instagram/post/views.py
@@ -1,5 +1,7 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
+from .forms import UploadFileForm
 from .models import Post
 
 
@@ -9,3 +11,22 @@ def post_list(request):
         'posts': posts,
     }
     return render(request, 'post/post_list.html', context)
+
+
+def handle_uploaded_file(request):
+    posts = Post.objects.all()
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'post/post_list.html', context)
+
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES['file'])
+            return HttpResponseRedirect('/success/url/')
+    else:
+        form = UploadFileForm()
+    return render(request, 'upload.html', {'form': form})
