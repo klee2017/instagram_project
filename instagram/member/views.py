8ee04d5484118e1b1eec328684cc0b46f10401df
Kyeong Lee <kaythechemist@gmail.com>
2017-10-17 11:31:32 +0900

    sign up added

--- instagram/member/views.py
+++ instagram/member/views.py
@@ -1,14 +1,23 @@
-from django.contrib.auth import logout, authenticate, login
-from django.contrib.auth.models import User
+from django.contrib.auth import logout, authenticate, login, get_user_model
 from django.http import HttpResponse
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
+
+User = get_user_model()
+
+
+# help 함수
 
 
 def signup(request):
-    username = request.POST['username']
-    password = request.POST['password']
-    user = User.objects.create_user(username, password)
-    return HttpResponse(username, password)
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        if username and password:
+            if User.objects.filter(username=username).exists():
+                return HttpResponse(f'Username {username} is already exist')
+            user = User.objects.create_user(username=username, password=password)
+            return HttpResponse(f'{user.username}, {user.password}')
+    return render(request, 'member/signup.html')
 
 
 def login_view(request):
