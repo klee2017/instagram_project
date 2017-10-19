261304a1e6d677c7cd471fa693853fd42d5efa5e
Kyeong Lee <kaythechemist@gmail.com>
2017-10-19 15:32:46 +0900

    AUTH USER MODEL settings

--- instagram/post/models.py
+++ instagram/post/models.py
@@ -1,12 +1,18 @@
+from django.conf import settings
 from django.db import models
 
 
 class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
     photo = models.ImageField(upload_to='post')
     created_at = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ['-created_at']
+
 
 class PostComment(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
     post = models.ForeignKey(Post, related_name='comments')
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
